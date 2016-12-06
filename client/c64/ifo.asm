570d48dd1ac4c2e9add2f85bb54b5b0b05f27ba3
Janne Johansson <janne.johansson@ipnett.com>
2016-12-06 20:44:20 +0100

    Fixup comments for kickassembler

--- client/c64/ifo.asm
+++ client/c64/ifo.asm
@@ -2,41 +2,40 @@ BasicUpstart2(start)
 
 	* = $0810 "Main"
 start:
-	jsr	loadmap
 
+	jsr	init
+	jsr	loadmap
 
 	rts
 
+init:	
+	
 loadmap:
         lda #fname_end-fname
         ldx #<fname
         ldy #>fname
-        jsr $FFBD     //; call SETNAM
+        jsr $FFBD     // call SETNAM
         lda #$01
         ldx $BA
-//#; last used dev number
+// last used dev number
         bne skip
-        ldx #$08      //; default to device 8
-skip:  ldy #$00      //; $00 means: load to new address
-        jsr $FFBA     //; call SETLFS
-
+        ldx #$08      // default to device 8
+skip:  ldy #$00      // $00 means: load to new address
+        jsr $FFBA     // call SETLFS
         ldx #<$1000
         ldy #>$1000
-        lda #$00      //; $00 means: load to memory (not verify)
-        jsr $FFD5     //; call LOAD
-        bcs error    //; if carry set, a load error has happened
+        lda #$00      // $00 means: load to memory (not verify)
+        jsr $FFD5     // call LOAD
+        bcs error    // if carry set, a load error has happened
         rts
 error:	
 
-
 //	; Accumulator contains BASIC error code
-
 //        ; most likely errors:
 //        ; A = $05 (DEVICE NOT PRESENT)
 //        ; A = $04 (FILE NOT FOUND)
 //        ; A = $1D (LOAD ERROR)
 //        ; A = $00 (BREAK, RUN/STOP has been pressed during loading)
-
 //        ... error handling ...
 	sta $d020
         rts
